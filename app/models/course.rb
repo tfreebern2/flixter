cb2f45e715e22c3d51b5be6fe557a3aaf1a838ed
tfreebern2 <tfreebern2@gmail.com>
2016-12-28 17:20:15 +0000

    List Courses and Set Controllers

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :title, :presence => { :message => "Title of Course is required" }
+  validates :description, :presence => { :message => "Description of Course is required" }
+  validates :cost, :presence => { :message => "Cost of Course is required" }, numericality: {greater_than_or_equal_to: 0}
 end
