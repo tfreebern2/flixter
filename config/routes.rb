2e5b4ee597455bbbbddca1622176fabd6826149f
tfreebern2 <tfreebern2@gmail.com>
2016-12-29 03:01:29 +0000

    Adding Lessons

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,14 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
+      end
     end
-  end
+    
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
