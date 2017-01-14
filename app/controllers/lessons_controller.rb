0c24113fde0df6431f8d1fbac5963d44309689a0
tfreebern2 <tfreebern2@gmail.com>
2017-01-14 02:14:56 +0000

    Next Lesson Button

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,23 +1,31 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:show]
+  before_action :current_user_enrolled, :only => [:show]
 
   def show
-    
   end
 
   private
+
   helper_method :current_lesson
 
-  def require_authorized_for_current_course
-    current_lesson.section.couse = current_lesson
-    if !current_user.nil? && current_user.enrolled_in?(current_lesson)
-      redirect_to course_path(current_lesson), alert: 'You must be enrolled to view this page'
-    end
-  end 
-
-  
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def current_course
+    current_section.course
+  end
+
+  def current_section
+    current_lesson.section
+  end
+
+  def current_user_enrolled
+    unless current_user.enrolled_in?(current_course)
+      error = "You are not enrolled in this course."
+      redirect_to course_path(current_course), alert: error
+    end
+  end
+
 end
